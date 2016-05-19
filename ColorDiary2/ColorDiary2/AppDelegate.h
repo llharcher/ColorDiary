4ec072e1c24bd4e66d96aefb6c08204098977bf0
llh <llhseed@163.com>
2016-05-19 20:17:10 +0800

    初始化.

---
+++ ColorDiary2/ColorDiary2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorDiary2
+//
+//  Created by apple on 16/5/19.
+//  Copyright (c) 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorDiary2/ColorDiary2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorDiary2
+//
+//  Created by apple on 16/5/19.
+//  Copyright (c) 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
